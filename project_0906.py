2c54b815593d052ab5f12d35d6d462f56ee11d76
swj3140 <seowoojin2112@naver.com>
2024-09-06 16:07:18 +0900

    read 함수 생성

--- project_0906.py
+++ project_0906.py
@@ -5,3 +5,10 @@ class phoneBook:
     def create(self, id, name, phone_number, email): # 연락처 추가
         self.contacts[id] = {'이름': name, '전화번호': phone_number, '이메일': email}
         print(f"ID {id}의 연락처가 추가되었습니다.")
+    
+    def read(self): # 전화번호 조회
+        if not self.contacts:
+            print("전화번호부가 비어있습니다.") # 전화번호부에 조회할 전화번호가 없을때
+        else:
+            for id, info in self.contacts.items():
+                print(f"ID: {id}, 이름: {info['이름']}, 전화번호: {info['전화번호']}, 이메일: {info['이메일']}")
