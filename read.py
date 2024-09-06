b0ffac201f10aa182dacc638734aaa5fe53c35fe
swj3140 <seowoojin2112@naver.com>
2024-09-06 17:14:37 +0900

    mysqlread

--- read.py
+++ read.py
@@ -1,6 +1,24 @@
-def read(self): # 전화번호부 출력
-        if not self.contacts:
-            print("전화번호부가 비어있습니다.") # 전화번호부에 조회할 전화번호가 없을때
+import mysql.connector
+
+class PhoneBook:
+    def __init__(self, db_config):
+        # 데이터베이스 연결 초기화
+        self.db_config = db_config
+        self.connection = mysql.connector.connect(**db_config)
+        self.cursor = self.connection.cursor(dictionary=True)
+    
+    def read(self):
+        # 연락처 정보를 가져오는 SQL 쿼리
+        query = "SELECT id, name, phone_number, email FROM contacts"
+        
+        # 쿼리 실행
+        self.cursor.execute(query)
+        
+        # 모든 행을 가져옴
+        contacts = self.cursor.fetchall()
+        
+        if not contacts:
+            print("전화번호부가 비어있습니다.")
         else:
-            for id, info in self.contacts.items():
-                print(f"ID: {id}, 이름: {info['이름']}, 전화번호: {info['전화번호']}, 이메일: {info['이메일']}")
+            for contact in contacts:
+                print(f"ID: {contact['id']}, 이름: {contact['name']}, 전화번호: {contact['phone_number']}, 이메일: {contact['email']}")
