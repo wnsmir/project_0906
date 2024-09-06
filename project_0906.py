d4d7d87346b4a820a1b29e51dbad5d48a14f9ca0
swj3140 <seowoojin2112@naver.com>
2024-09-06 16:22:14 +0900

    read 함수 수정

--- project_0906.py
+++ project_0906.py
@@ -61,7 +61,7 @@ class phoneBook:
         self.contacts[id] = {'이름': name, '전화번호': phone_number, '이메일': email}
         print(f"ID {id}의 연락처가 추가되었습니다.")
 
-    def display(self): # 전화번호부 출력
+    def read(self): # 전화번호부 출력
         if not self.contacts:
             print("전화번호부가 비어있습니다.") # 전화번호부에 조회할 전화번호가 없을때
         else:
